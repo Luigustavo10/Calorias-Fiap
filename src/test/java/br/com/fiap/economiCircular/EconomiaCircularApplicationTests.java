c9b09f457bf09cc511ebdc664080d12845d13c6e
Luis Gustavo <araujodasilvaluisgustavo@gmail.com>
2025-05-13 20:10:35 -0300

    Finalizando Project

--- src/test/java/br/com/fiap/economiCircular/EconomiaCircularApplicationTests.java
+++ src/test/java/br/com/fiap/economiCircular/EconomiaCircularApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CaloriasApplicationTests {
+class EconomiaCircularApplicationTests {
 
 	@Test
 	void contextLoads() {
