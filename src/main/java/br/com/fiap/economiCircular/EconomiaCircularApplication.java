2263cb1d723e5acbc6180f5ba5d63960e1ca6902
Luis Gustavo <araujodasilvaluisgustavo@gmail.com>
2025-05-13 20:09:14 -0300

    Finalizando Project

--- src/main/java/br/com/fiap/economiCircular/EconomiaCircularApplication.java
+++ src/main/java/br/com/fiap/economiCircular/EconomiaCircularApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CaloriasApplication {
+public class EconomiaCircularApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CaloriasApplication.class, args);
+		SpringApplication.run(EconomiaCircularApplication.class, args);
 	}
 
 }
